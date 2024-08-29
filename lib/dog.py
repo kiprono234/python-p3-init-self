d7e659ab358fbbee59d631bf7e078fca490e2c82
kevin <kipronok234@gmail.com>
2024-08-29 17:21:30 +0300

    python3-p3

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed='Mutt'):
+        self.name = name
+        self.breed = breed
+
+
+        def bark(self):
+            print("Woof!")
+
+            def sit(self):
+                print("The dog is sitting.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+        
+        
+        
+
     pass
